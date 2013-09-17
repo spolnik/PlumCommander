e46ec76b020864b9ad3f46cba639cc1ad5e64398
Jacek Spólnik <jacek.spolnik@gmail.com>
2013-09-17 15:01:16 +0200

    Add skeleton of PlumCommander + PlumDictionary implementation

---
+++ src/main/java/com/plumcommander/engine/PlumDirectory.java
@@ -0,0 +1,124 @@
+package com.plumcommander.engine;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static com.google.common.base.Preconditions.*;
+
+public final class PlumDirectory {
+
+    private Path currentPath;
+
+    public PlumDirectory() throws IOException {
+        currentPath = Paths.get(".").toRealPath();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String command = null;
+        PlumDirectory plumDirectory = new PlumDirectory();
+
+        do {
+
+            System.out.print("Command (LS, CD, PWD, EXIT): ");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String path = ".";
+
+            try {
+                String commandLine = br.readLine();
+                String[] commandArgs = commandLine.split(" ");
+
+                command = commandArgs[0].toUpperCase();
+
+                if (commandArgs.length > 1)
+                    path = commandArgs[1];
+
+                switch (command) {
+                    case "CD":
+                        plumDirectory.changeDirectory(path);
+                        break;
+                    case "PWD":
+                        System.out.println(plumDirectory.getCurrentPath());
+                        break;
+                    case "LS":
+                        for (File file : plumDirectory.getFiles())
+                            System.out.println(file.getName());
+                        break;
+                    case "EXIT":
+                        System.out.println("Existing from program.");
+                        break;
+                    default:
+                        System.err.println("Wrong command: " + commandArgs[0]);
+                        break;
+                }
+
+            } catch (IOException ioe) {
+                System.err.println(ioe.getMessage());
+            }
+
+        } while (!Objects.equals(command, "EXIT"));
+    }
+
+    String getCurrentPath() {
+        return currentPath.toString();
+    }
+
+    void changeDirectory(String directoryPath) throws IOException {
+        checkArgument(Strings.isNullOrEmpty(directoryPath), "Directory path cannot be null nor empty");
+
+        Path path = getRealPath(directoryPath);
+
+        if (!Files.exists(path))
+            throw new FileNotFoundException("File doesn't exist: " + path.toString());
+
+        if (Files.isDirectory(path))
+            currentPath = path;
+        else
+            throw new IOException("File is not directory!");
+
+    }
+
+    List<File> getFiles() {
+        File[] files = currentPath.toFile().listFiles();
+
+        if (files == null)
+            return Lists.newArrayList();
+
+        Arrays.sort(files, getFilesComparator());
+        return Arrays.asList(files);
+    }
+
+    private Path getRealPath(String directoryPath) throws IOException {
+        boolean isAbsolute = new File(directoryPath).isAbsolute();
+
+        return (isAbsolute
+                ? Paths.get(directoryPath)
+                : Paths.get(currentPath.toString(), directoryPath)
+        ).toRealPath();
+    }
+
+    private Comparator<File> getFilesComparator() {
+        return new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                if (f1.isDirectory() && !f2.isDirectory()) {
+                    // Directory before non-directory
+                    return -1;
+                } else if (!f1.isDirectory() && f2.isDirectory()) {
+                    // Non-directory after directory
+                    return 1;
+                } else {
+                    // Alphabetic order otherwise
+                    return f1.compareTo(f2);
+                }
+            }
+        };
+    }
+}
---
+++ src/test/java/com/plumcommander/engine/PlumDirectoryTest.java
@@ -0,0 +1,73 @@
+package com.plumcommander.engine;
+
+import junit.framework.Assert;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * User: jacek
+ * Date: 9/17/13
+ * Time: 11:02 AM
+ */
+public class PlumDirectoryTest {
+
+    public static final String PATH_NAME = "test_basic_scenario";
+
+    public static final String PATH_NAME_2 = "test_deeper_scenario";
+
+    private static PlumDirectory plumDirectory;
+
+    @BeforeClass
+    public static void testSetUp() throws Exception {
+        new File(PATH_NAME).mkdir();
+
+        Paths.get(PATH_NAME, "text1.txt").toFile().createNewFile();
+        Paths.get(PATH_NAME, "text2.txt").toFile().createNewFile();
+
+        Paths.get(PATH_NAME, PATH_NAME_2).toFile().mkdir();
+        Paths.get(PATH_NAME, PATH_NAME_2, "textA.txt").toFile().createNewFile();
+    }
+
+    @AfterClass
+    public static void testTearDown() throws Exception {
+        FileUtils.deleteDirectory(new File(PATH_NAME));
+    }
+
+    @Before
+    public void before() throws Exception {
+        plumDirectory = new PlumDirectory();
+        plumDirectory.changeDirectory(PATH_NAME);
+    }
+
+    @Test
+    public void testGetFiles() throws Exception {
+        List<File> files = plumDirectory.getFiles();
+
+        Assert.assertEquals(files.size(), 3);
+    }
+
+    @Test
+    public void testGetCurrentPath() throws Exception {
+        String currentPath = plumDirectory.getCurrentPath();
+
+        Assert.assertTrue(currentPath.endsWith(PATH_NAME));
+    }
+
+    @Test
+    public void testChangeDirectory() throws Exception {
+        plumDirectory.changeDirectory(PATH_NAME_2);
+
+        String currentPath = plumDirectory.getCurrentPath();
+        Assert.assertTrue(currentPath.endsWith(PATH_NAME_2));
+
+        List<File> files = plumDirectory.getFiles();
+        Assert.assertEquals(files.size(), 1);
+    }
+}

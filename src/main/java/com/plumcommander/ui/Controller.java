7a344a553e8ec8d195f3e3d5d391961dbf363bb9
Jacek Spólnik <jacek.spolnik@gmail.com>
2013-09-22 06:57:19 +0200

    - Add new resources (background, stylesheet css, fxml) - update JavaFX app (sample)

--- src/main/java/com/plumcommander/ui/Controller.java
+++ src/main/java/com/plumcommander/ui/Controller.java
@@ -1,4 +1,13 @@
 package com.plumcommander.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML private Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent actionEvent) {
+        actiontarget.setText("Sign in button pressed");
+    }
 }
